266dd83d9ed3e109cfcea2674945303a0543289f
Tristan Isham <tristan@isham.co>
2022-11-08 23:35:54 -0500

    added system information parsing

--- src/main.zig
+++ src/main.zig
@@ -50,16 +50,23 @@ pub fn main() !void {
             const tree = try version.parseVersionJSON(&response_buffer, &arena_state);
 
             if (tree.root.Object.get(user_ver)) |value| {
-                value.dump();
+                var info = try getSystemInfo();
+                var buf: [100]u8 = undefined;
+                var buf_slice: []u8 = undefined;
+
+                if (streql(info.arch,  "x86")) {
+                    buf_slice = try std.fmt.bufPrint(&buf, "{s}-{s}", .{ "x86_64", info.tag });
+                } else {
+                    buf_slice = try std.fmt.bufPrint(&buf, "{s}-{s}", .{ info.arch, info.tag });
+                }
+
+                std.debug.print("{}", .{value.Object.get(buf_slice).?.Object.get("tarball").?});
+
             } else {
                 std.debug.print("Invalid Zig version provided. Try master or latest-stable\n", .{});
                 return;
             }
-            std.debug.print("{s}\n", .{res.positionals[i + 1]});
-            var info = try NativeTargetInfo.detect(.{});
-            //  std.log.debug("{}", .{info});
-            std.log.debug("arch: {}", .{info.target.cpu.arch});
-            std.log.debug("os:   {}", .{info.target.os.tag});
+
             return;
         } else if (streql("use", val) and res.positionals.len >= i + 1) {}
     }
@@ -72,6 +79,62 @@ test "simple test" {
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
 
+/// Compares two strings. Returns a bool based on their equality.
 fn streql(original: []const u8, compto: []const u8) bool {
     return std.mem.eql(u8, original, compto);
+}
+
+const SystemInfo = struct { arch: []const u8, tag: []const u8 };
+
+fn getSystemInfo() !SystemInfo {
+    const info = try NativeTargetInfo.detect(.{});
+    const arch = info.target.cpu.arch.genericName();
+    const tag = switch (info.target.os.tag) {
+        .ananas => "ananas",
+        .cloudabi => "cloudabi",
+        .dragonfly => "dragonfly",
+        .freebsd => "freebsd",
+        .fuchsia => "fuchsia",
+        .ios => "ios",
+        .kfreebsd => "kfreebsd",
+        .linux => "linux",
+        .lv2 => "lv2",
+        .macos => "macos",
+        .netbsd => "netbsd",
+        .openbsd => "openbsd",
+        .solaris => "solaris",
+        .windows => "windows",
+        .zos => "zos",
+        .haiku => "haiku",
+        .minix => "minix",
+        .rtems => "rtems",
+        .nacl => "nacl",
+        .aix => "aix",
+        .cuda => "cuda",
+        .nvcl => "nvcl",
+        .amdhsa => "amdhsa",
+        .ps4 => "ps4",
+        .ps5 => "ps5",
+        .elfiamcu => "elfiamcu",
+        .tvos => "tvos",
+        .watchos => "watchos",
+        .driverkit => "driverkit",
+        .mesa3d => "mesa3d",
+        .contiki => "contiki",
+        .amdpal => "amdpal",
+        .hermit => "hermit",
+        .hurd => "hurd",
+        .wasi => "wasi",
+        .emscripten => "emscripten",
+        .shadermodel => "shadermodel",
+        .uefi => "uefi",
+        .opencl => "opencl",
+        .glsl450 => "glsl450",
+        .vulkan => "vulkan",
+        .plan9 => "plan9",
+        .other => "other",
+        .freestanding => "freestanding",
+    };
+
+    return SystemInfo{ .arch = arch, .tag = @as([]const u8, tag) };
 }

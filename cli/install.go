93efdca67988275152f2f75392b8d2934dd4384a
Tristan Isham <tristan.isham@hey.com>
2022-11-28 01:30:11 -0500

    halfway done to replicating Zig version

--- cli/install.go
+++ cli/install.go
@@ -2,10 +2,12 @@ package cli
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 func Install(version string) error {
@@ -14,7 +16,7 @@ func Install(version string) error {
 		homedir = "~"
 	}
 	zvm := filepath.Join(homedir, ".zvm")
-	os.Mkdir(zvm, 0755) 
+	os.Mkdir(zvm, 0755)
 
 	req, err := http.NewRequest("GET", "https://ziglang.org/download/index.json", nil)
 	if err != nil {
@@ -37,16 +39,65 @@ func Install(version string) error {
 		return err
 	}
 
-	rawVersionStructure := make (map[string]any)
+	rawVersionStructure := make(map[string]map[string]any)
 	if err := json.Unmarshal(versions, &rawVersionStructure); err != nil {
 		return err
 	}
 
+	tarPath, err := getTarPath(version, &rawVersionStructure)
+	if err != nil {
+		return err
+	}
 
-	return installVersion(rawVersionStructure)
-}
+	tarReq, err := http.Get(*tarPath)
+	if err != nil {
+		return err
+	}
+	defer tarReq.Body.Close()
+	_ = os.MkdirAll(filepath.Join(zvm, version), 0755)
+	tarDownloadPath := filepath.Join(zvm, version, fmt.Sprintf("%s.tar.xz", version))
+	out, err := os.Create(tarDownloadPath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	written, err := io.Copy(out, tarReq.Body)
+	if err != nil {
+		return err
+	}
 
-func installVersion(data map[string]any) error {
+	fmt.Printf("Wrote %d megabytes to %s\n", written/1000, tarDownloadPath)
 
 	return nil
+
+}
+
+func getTarPath(version string, data *map[string]map[string]any) (*string, error) {
+	if info, ok := (*data)[version]; ok {
+		arch, ops := zigStyleSysInfo()
+		if systemInfo, ok := info[fmt.Sprintf("%s-%s", arch, ops)]; ok {
+			if base, ok := systemInfo.(map[string]any); ok {
+				if tar, ok := base["tarball"].(string); ok {
+					return &tar, nil
+				}
+			} else {
+				return nil, fmt.Errorf("unable to find necessary download path")
+			}
+		} else {
+			return nil, fmt.Errorf("invalid/unsupported system: ARCH: %s OS: %s", arch, ops)
+		}
+	}
+	return nil, fmt.Errorf("invalid Zig version: %s", version)
+}
+
+func zigStyleSysInfo() (string, string) {
+	var arch string
+	switch runtime.GOARCH {
+	case "amd64":
+		arch = "x86_64"
+	default:
+		arch = runtime.GOARCH
+	}
+
+	return arch, runtime.GOOS
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 	"zvm/cli"
 )
@@ -11,7 +12,9 @@ func main() {
 		switch arg {
 		case "install", "i":
 			if len(args) > i+1 {
-				cli.Install(args[i+1])
+				if err := cli.Install(args[i+1]); err != nil {
+					log.Fatal(err)
+				}
 			}
 		}
 	}

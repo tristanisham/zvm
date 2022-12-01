1658b43e58ee26dced8343df3e4d41dcc04fbf8a
Tristan Isham <tristan@isham.co>
2022-11-30 19:08:58 -0500

    another update

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"zvm/cli"
 )
 
 func main() {
+	zvm := cli.Initialize()
 	args := os.Args[1:]
 	for i, arg := range args {
 		switch arg {
 		case "install", "i":
 			if len(args) > i+1 {
-				if err := cli.Install(args[i+1]); err != nil {
+				if err := zvm.Install(args[i+1]); err != nil {
 					log.Fatal(err)
 				}
+			}
+		case "use":
+			if len(args) > i+1 {
+				if err := zvm.Use(args[i+1]); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(*zvm)
 			}
 		}
 	}

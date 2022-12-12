da62c5d6496fa1c80988eb58bd8ab54e83b1cbcc
Tristan Isham <tristan@isham.co>
2022-12-12 02:03:27 -0500

    update version string

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 				}
 			}
 		case "version", "--version", "-v":
-			fmt.Println("zvm v0.1.2")
+			fmt.Println("zvm v0.1.3")
 			return
 		case "help", "--help", "-h":
 			var help string

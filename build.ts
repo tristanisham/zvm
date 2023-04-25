6f02a610ffeecffad9ed3ace1e06bdb1eaf8b9f2
Tristan Isham <tristan@isham.co>
2023-04-25 17:20:01 -0400

    updated build script

--- build.ts
+++ build.ts
@@ -1,7 +1,10 @@
 import { exec } from "https://deno.land/x/exec@0.0.5/mod.ts";
-// import { parse } from "https://deno.land/std@0.167.0/flags/mod.ts";
+import * as zip from "https://deno.land/x/zipjs@v2.7.6/index.js";
+import { Tar } from "https://deno.land/std@0.184.0/archive/mod.ts";
+import { copy } from "https://deno.land/std@0.184.0/streams/copy.ts";
 
-// const args = parse(Deno.args)
+
+
 
 const GOARCH = [
     "amd64", "arm64"
@@ -21,6 +24,7 @@ for (const os of GOOS) {
         if (os == "solaris" && ar == "arm64" || os == "plan9" && ar == "arm64") continue;
         Deno.env.set("GOOS", os)
         Deno.env.set("GOARCH", ar)
+        // Deno.env.set("CGO_ENABLED", "1")
         const zvm_str = `zvm-${os}-${ar}`
         console.time(`Build zvm: ${zvm_str}`)
         await exec(`go build -o build/${zvm_str}/zvm`)
@@ -36,14 +40,24 @@ for (const os of GOOS) {
 
         if (os == "windows") {
             console.time(`Compress zvm: ${zvm_str}`)
-            await exec(`zip ${zvm_str}.zip ${zvm_str}`)
+            const zipWriter = new zip.BlobWriter();
+            const output = new zip.TextReader(await Deno.readTextFile(`${zvm_str}/zvm`))
+            const zipper = new zip.ZipWriter(zipWriter);
+            await zipper.add("zvm", output)
             console.timeEnd(`Compress zvm: ${zvm_str}`)
             continue;
         }
 
+        const tar = new Tar()
         console.time(`Compress zvm: ${zvm_str}`)
-        await exec(`tar -czf ${zvm_str}.tar.gz ${zvm_str}`)
+        await tar.append("zvm", {
+            filePath: `${zvm_str}/zvm`
+        })
+        const writer = await Deno.open(`./${zvm_str}.tar`, { write: true, create: true })
+        await copy(tar.getReader(), writer);
+        writer.close();
         console.timeEnd(`Compress zvm: ${zvm_str}`)
+
     }
 }
 

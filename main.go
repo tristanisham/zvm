30ef429c3caaff8ef71a521befe92863f171ff55
OlshaMB <michaelolshanskiy@icloud.com>
2024-01-31 09:00:30 +0300

    fix: Fixes the #42 bug

--- main.go
+++ main.go
@@ -43,22 +43,15 @@ func main() {
 	lsRemote := lsFlagSet.Bool("all", false, "List all available versions of Zig to install")
 
 	// Global config
-	sVersionMapUrl := flag.String("vmu", "https://ziglang.org/download/index.json", "Set ZVM's version map URL for custom Zig distribution servers")
+	sVersionMapUrl := flag.String("vmu", "default", "Set ZVM's version map URL for custom Zig distribution servers")
 	sColorToggle := flag.Bool("color", true, "Turn on or off ZVM's color output")
 	flag.Parse()
 
-	if sVersionMapUrl != nil {
+	if *sVersionMapUrl != "default"  {
 		log.Warn("this is a beta flag, and may not behave as expected.")
-		if *sVersionMapUrl == "default" {
-			if err := zvm.Settings.ResetVersionMap(); err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			if err := zvm.Settings.SetVersionMapUrl(*sVersionMapUrl); err != nil {
-				log.Fatal(err)
-			}
+		if err := zvm.Settings.SetVersionMapUrl(*sVersionMapUrl); err != nil {
+			log.Fatal(err)
 		}
-
 	}
 
 	if sColorToggle != nil {

340037dad9e7e45b0858fe9a5b15e67b2320c919
Nithin K Joy <nithinjoy121212@gmail.com>
2024-03-06 10:28:04 +0530

    moved windows permission logic to new file

--- cli/fileperms_win.go
+++ cli/fileperms_win.go
@@ -7,9 +7,7 @@
 package cli
 
 import (
-	"bytes"
 	"os"
-	"os/exec"
 )
 
 func canModifyFile(path string) (bool, error) {
@@ -44,27 +42,4 @@ func canModifyFile(path string) (bool, error) {
 	// }
 
 	return false, nil
-}
-
-func elevatedRun(name string, arg ...string) (bool, error) {
-	ok, err := run("cmd", nil, append([]string{"/C", name}, arg...)...)
-	if err != nil {
-		ok, err = run("elevate.cmd", nil, append([]string{"cmd", "/C", name}, arg...)...)
-	}
-	return ok, err
-}
-
-func run(name string, dir *string, arg ...string) (bool, error) {
-	c := exec.Command(name, arg...)
-	if dir != nil {
-		c.Dir = *dir
-	}
-	var stderr bytes.Buffer
-	c.Stderr = &stderr
-	err := c.Run()
-	if err != nil {
-		return false, err
-	}
-
-	return true, nil
 }

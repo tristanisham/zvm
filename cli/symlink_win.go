06def22a88ff4f7f2f6d163dc46a0dcdcb873820
Tristan Isham <tristan@isham.co>
2024-03-25 23:33:10 -0400

    Okay, so the secret is actually doing some development on Windows

--- cli/symlink_win.go
+++ cli/symlink_win.go
@@ -2,9 +2,14 @@ package cli
 
 import (
 	"bytes"
+	"os"
 	"os/exec"
+	"strings"
+
+	"syscall"
 
 	"github.com/charmbracelet/log"
+	"golang.org/x/sys/windows"
 )
 
 // winElevatedRun elevates a process to run as an administrator on Windows.
@@ -31,4 +36,31 @@ func run(name string, dir *string, arg ...string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func becomeAdmin() error {
+	verb := "runas"
+	exe, _ := os.Executable()
+	cwd, _ := os.Getwd()
+	args := strings.Join(os.Args[1:], " ")
+
+	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+	exePtr, _ := syscall.UTF16PtrFromString(exe)
+	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+	argPtr, _ := syscall.UTF16PtrFromString(args)
+
+	var showCmd int32 = 1 //SW_NORMAL
+
+	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+	if err != nil {
+		return err
+	}
+	
+	return nil
+}
+
+func checkAdmin() bool {
+	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+
+	return err == nil
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.22
 
 require (
 	github.com/charmbracelet/lipgloss v0.10.0
-	github.com/charmbracelet/log v0.3.1
+	github.com/charmbracelet/log v0.4.0
 	github.com/schollz/progressbar/v3 v3.14.2
 	github.com/tristanisham/clr v0.0.0-20221004001624-00ee60046d85
 	golang.org/x/mod v0.16.0
@@ -20,7 +20,7 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/exp v0.0.0-20240314144324-c7f7c6466f7f // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/term v0.18.0 // indirect
 )

06ec8f3efdaf71507ef88fdde1e79786b6b0e46e
Tristan Isham <tristan@isham.co>
2024-03-27 21:48:08 -0400

    Update symlink_win.go
    Switched to using system calls instead of  escalation

--- cli/meta/symlink_win.go
+++ cli/meta/symlink_win.go
@@ -9,63 +9,73 @@ import (
 	// "bytes"
 	"errors"
 	"os"
-	"runtime"
 
 	// "os/exec"
-	"strings"
+	// "strings"
 
 	"syscall"
-
 	// "github.com/charmbracelet/log"
-	"golang.org/x/sys/windows"
+	// "golang.org/x/sys/windows"
 )
 
+// func becomeAdmin() error {
+// 	verb := "runas"
+// 	exe, _ := os.Executable()
+// 	cwd, _ := os.Getwd()
+// 	args := strings.Join(os.Args[1:], " ")
 
-func becomeAdmin() error {
-	verb := "runas"
-	exe, _ := os.Executable()
-	cwd, _ := os.Getwd()
-	args := strings.Join(os.Args[1:], " ")
+// 	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+// 	exePtr, _ := syscall.UTF16PtrFromString(exe)
+// 	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+// 	argPtr, _ := syscall.UTF16PtrFromString(args)
 
-	verbPtr, _ := syscall.UTF16PtrFromString(verb)
-	exePtr, _ := syscall.UTF16PtrFromString(exe)
-	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
-	argPtr, _ := syscall.UTF16PtrFromString(args)
+// 	var showCmd int32 = 1 //SW_NORMAL
 
-	var showCmd int32 = 1 //SW_NORMAL
+// 	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
-	if err != nil {
-		return err
-	}
+// 	return nil
+// }
 
-	return nil
-}
+// func isAdmin() bool {
+// 	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
 
-func isAdmin() bool {
-	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
-
-	return err == nil
-}
+// 	return err == nil
+// }
 
 // Symlink is a wrapper around Go's os.Symlink,
 // but with automatic privilege escalation on windows
 // for systems that do not support non-admin symlinks.
 func Symlink(oldname, newname string) error {
-	if err := os.Symlink(oldname, newname); err != nil {
+	err := os.Symlink(oldname, newname)
+	if err != nil {
 
 		if errors.Is(err, &os.LinkError{}) {
-			if runtime.GOOS == "windows" {
-				if !isAdmin() {
-					if err := becomeAdmin(); err != nil {
-						if err := os.Symlink(oldname, newname); err != nil {
-							return errors.Join(ErrEscalatedSymlink, err)
-						}
-					} else {
-						return errors.Join(ErrWinEscToAdmin, err)
-					}
-				}
+			var flags uint32
+
+			if stat, _ := os.Stat(oldname); stat.IsDir() {
+				flags |= syscall.SYMBOLIC_LINK_FLAG_DIRECTORY
 			}
+
+			flags |= 0x02 // SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE
+			ptrOldName, err := syscall.UTF16PtrFromString(oldname)
+			if err != nil {
+				return err
+			}
+
+			ptrNewName, err := syscall.UTF16PtrFromString(newname)
+			if err != nil {
+				return err
+			}
+
+			err = syscall.CreateSymbolicLink(ptrNewName, ptrOldName, flags)
+			if err != nil {
+				return &os.LinkError{Op: "symlink", Old: oldname, New: newname, Err: err}
+			}
+		} else {
+			return err
 		}
 
 	}

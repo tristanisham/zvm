2b1050787a08258c2de8a86fc763f7407ad9423b
Tristan Isham <tristan@isham.co>
2024-04-10 17:46:25 -0400

    Create use_test.go
    Test for if we can find Symlinks

---
+++ cli/use_test.go
@@ -0,0 +1,19 @@
+package cli
+
+import (
+	"os"
+	"testing"
+)
+
+func TestSymlinkExists(t *testing.T) {
+	if err := os.Symlink("use_test.go", "symlink.test"); err != nil {
+		t.Error(err)
+	}
+
+	stat, err := os.Lstat("symlink.test");
+	if err != nil {
+		t.Errorf("%q: %s", err, stat.Name())
+	}
+
+	defer os.Remove("symlink.test")
+}

aeb25b8d062b7b356aa9ca210d10bcfa001d0ad3
Yoshikage Kira <8bit_yoshikage_kira@proton.me>
2024-07-14 01:54:45 -0400

    fix: Add missing -e flag on echo statements.
    - FIx inaccurate install progress statements

--- install.sh
+++ install.sh
@@ -15,12 +15,25 @@ fi
 # echo "Installing zvm-$OS-$ARCH"
 
 install_latest() {
-    echo -e "Installing $1 in $(pwd)/zvm"
     if [ "$(uname)" = "Darwin" ]; then
-        # Do something under MacOS platform
+        # MacOS platform
+        install_dir="$HOME/Library/zvm/self"
+        bin_dir="$HOME/bin"
+    elif [ "$OS" = "Linux" ]; then
+        # GNU/Linux platform
+        install_dir="$XDG_DATA_HOME/zvm/self"
+        bin_dir="$HOME/.local/bin"
+    else
+        # Other OSes
+        install_dir="$HOME/.zvm"
+        bin_dir="$HOME/.zvm/bin"
+    fi
 
+    echo -e "Installing $1 in $install_dir"
+
+    if [ "$(uname)" = "Darwin" ]; then
+        # MacOS platform
         if command -v wget >/dev/null 2>&1; then
-
             echo "wget is installed. Using wget..."
             wget -q --show-progress --max-redirect 5 -O zvm.tar "https://github.com/tristanisham/zvm/releases/latest/download/$1"
         else
@@ -28,15 +41,14 @@ install_latest() {
             curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$1" -o zvm.tar
         fi
 
-        mkdir -p $HOME/Library/zvm/self
-        tar -xf zvm.tar -C $HOME/Library/zvm/self
-        ln -s $HOME/Library/zvm/self/zvm $HOME/bin
+        mkdir -p "$install_dir"
+        tar -xf zvm.tar -C "$install_dir"
+        ln -s "$install_dir/zvm" "$bin_dir"
         rm "zvm.tar"
 
-    elif [ $OS = "Linux" ]; then
-        # Do something under GNU/Linux platform
+    elif [ "$OS" = "Linux" ]; then
+        # GNU/Linux platform
         if command -v wget >/dev/null 2>&1; then
-
             echo "wget is installed. Using wget..."
             wget -q --show-progress --max-redirect 5 -O zvm.tar "https://github.com/tristanisham/zvm/releases/latest/download/$1"
         else
@@ -44,18 +56,20 @@ install_latest() {
             curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$1" -o zvm.tar
         fi
 
-        mkdir -p $XDG_DATA_HOME/zvm/self
-        tar -xf zvm.tar -C $XDG_DATA_HOME/zvm/self
-        ln -s $XDG_DATA_HOME/zvm/self/zvm $HOME/.local/bin
+        mkdir -p "$install_dir"
+        tar -xf zvm.tar -C "$install_dir"
+        ln -s "$install_dir/zvm" "$bin_dir"
         rm "zvm.tar"
-    elif [ $OS = "MINGW32_NT" ]; then
-        # Do something under 32 bits Windows NT platform
-        curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$($1)" -o zvm.zip
 
-    elif [ $OS == "MINGW64_NT" ]; then
-        # Do something under 64 bits Windows NT platform
-        curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$($1)" -o zvm.zip
+    elif [ "$OS" = "MINGW32_NT" ]; then
+        # 32 bits Windows NT platform
+        curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$1" -o zvm.zip
+        # Add extraction and installation commands as needed
 
+    elif [ "$OS" = "MINGW64_NT" ]; then
+        # 64 bits Windows NT platform
+        curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$1" -o zvm.zip
+        # Add extraction and installation commands as needed
     fi
 }
 
@@ -88,8 +102,8 @@ if [[ "$TERM" == "xterm" || "$TERM" == "xterm-256color" || "$TERM" == "screen" |
     echo -e "${GREEN}echo${NC} ${RED}'export ZVM_PATH=\"\$XDG_DATA_HOME/zvm\"'${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC} ${GREEN}# Linux${NC}"
     echo -e "${GREEN}echo${NC} ${RED}'export ZVM_PATH=\"\$HOME/Library/zvm\"'${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC} ${GREEN}# macOS${NC}"
 
-    echo "Make sure ${RED}\$HOME/.local/bin${NC} is in your PATH on Linux."
-    echo "Make sure ${RED}\$HOME/bin${NC} is in your PATH on macOS."
+    echo -e "Make sure ${RED}\$HOME/.local/bin${NC} is in your PATH on Linux."
+    echo -e "Make sure ${RED}\$HOME/bin${NC} is in your PATH on macOS."
 
     echo "Run 'zvm i master' to install Zig"
 else

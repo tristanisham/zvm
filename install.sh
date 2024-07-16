63b6dc94b327fdc95d83742f41f5471c9ae1992d
Yoshikage Kira <8bit_yoshikage_kira@proton.me>
2024-07-16 01:37:19 -0400

    refactor: Clean up the install.sh script

--- install.sh
+++ install.sh
@@ -1,122 +1,139 @@
 #!/usr/bin/env bash
 
 # ZVM install script - v0.1.5 - ZVM: https://github.com/tristanisham/zvm
-
 ARCH=$(uname -m)
 OS=$(uname -s)
 
-if [ $ARCH = "aarch64" ]; then
-    ARCH="arm64"
-fi
-if [ $ARCH = "x86_64" ]; then
-    ARCH="amd64"
-fi
+post_install_setup() {
+    if [[ "$TERM" == "xterm" || "$TERM" == "xterm-256color" || "$TERM" == "screen" || "$TERM" == "tmux" ]]; then
+        # Colors
+        RED='\033[0;31m'   # For errors
+        GREEN='\033[0;32m' # For commands
+        BLUE='\033[0;34m'  # For variables
+        NC='\033[0m'       # No Color
 
-# echo "Installing zvm-$OS-$ARCH"
+        echo -e "${GREEN}To complete the setup, add the following lines to your .bashrc or .profile file:${NC}"
 
-install_latest() {
-    if [ "$(uname)" = "Darwin" ]; then
-        # MacOS platform
-        install_dir="$HOME/Library/zvm/self"
-        bin_dir="$HOME/bin"
-    elif [ "$OS" = "Linux" ]; then
-        # GNU/Linux platform
-        install_dir="$XDG_DATA_HOME/zvm/self"
-        bin_dir="$HOME/.local/bin"
+        echo
+        echo -e "${BLUE}# ZVM${NC}"
+
+        if [[ "$OS" == "Linux" ]]; then
+            # Linux Instructions
+            echo -e "export ZVM_PATH=\"${BLUE}\$XDG_DATA_HOME/zvm${NC}\""
+            echo -e "export PATH=\"\${PATH}:${BLUE}\$ZVM_PATH/bin${NC}\""
+            echo -e "${GREEN}Ensure that ${BLUE}\$HOME/.local/bin${NC} is in your PATH on Linux."
+        elif [[ "$OS" == "Darwin" ]]; then
+            # macOS Instructions
+            echo -e "export ZVM_PATH=\"${BLUE}\$HOME/Library/zvm${NC}\""
+            echo -e "export PATH=\"\${PATH}:${BLUE}\$ZVM_PATH/bin${NC}\""
+            echo -e "${GREEN}Ensure that ${BLUE}\$HOME/bin${NC} is in your PATH on macOS."
+        else
+            # OS not supported
+            echo -e "${RED}Your OS ($OS) is not supported by this script.${NC}"
+        fi
     else
-        # Other OSes
-        install_dir="$HOME/.zvm"
-        bin_dir="$HOME/.zvm/bin"
+        echo "To complete the setup, add the following lines to your .bashrc or .profile file:"
+
+        echo
+        echo "# ZVM"
+
+        if [[ "$OS" == "Linux" ]]; then
+            # Linux Instructions
+            echo "export ZVM_PATH=\"\$XDG_DATA_HOME/zvm\""
+            echo "export PATH=\"\$PATH:\$ZVM_PATH/bin\""
+            echo "Ensure that \$HOME/.local/bin is in your PATH on Linux."
+        elif [[ "$OS" == "Darwin" ]]; then
+            # macOS Instructions
+            echo "export ZVM_PATH=\"\$HOME/Library/zvm\""
+            echo "export PATH=\"\$PATH:\$ZVM_PATH/bin\""
+            echo "Ensure that \$HOME/bin is in your PATH on macOS."
+        else
+            # OS not supported
+            echo "Your OS ($OS) is not supported by this script."
+        fi
     fi
 
-    echo -e "Installing $1 in $install_dir"
+    echo
+    echo "Run 'zvm i master' to install Zig"
+}
 
-    if [ "$(uname)" = "Darwin" ]; then
-        # MacOS platform
-        if command -v wget >/dev/null 2>&1; then
-            echo "wget is installed. Using wget..."
-            wget -q --show-progress --max-redirect 5 -O zvm.tar "https://github.com/tristanisham/zvm/releases/latest/download/$1"
-        else
-            echo "wget is not installed. Using curl..."
-            curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$1" -o zvm.tar
-        fi
+download_file() {
+    local url="$1"
+    local output_filename="$2"
 
-        mkdir -p "$install_dir"
-        tar -xf zvm.tar -C "$install_dir"
-        ln -s "$install_dir/zvm" "$bin_dir"
-        rm "zvm.tar"
-
-    elif [ "$OS" = "Linux" ]; then
-        # GNU/Linux platform
-        if command -v wget >/dev/null 2>&1; then
-            echo "wget is installed. Using wget..."
-            wget -q --show-progress --max-redirect 5 -O zvm.tar "https://github.com/tristanisham/zvm/releases/latest/download/$1"
-        else
-            echo "wget is not installed. Using curl..."
-            curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$1" -o zvm.tar
-        fi
-
-        mkdir -p "$install_dir"
-        tar -xf zvm.tar -C "$install_dir"
-        ln -s "$install_dir/zvm" "$bin_dir"
-        rm "zvm.tar"
-
-    elif [ "$OS" = "MINGW32_NT" ]; then
-        # 32 bits Windows NT platform
-        curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$1" -o zvm.zip
-        # Add extraction and installation commands as needed
-
-    elif [ "$OS" = "MINGW64_NT" ]; then
-        # 64 bits Windows NT platform
-        curl -L --max-redirs 5 "https://github.com/tristanisham/zvm/releases/latest/download/$1" -o zvm.zip
-        # Add extraction and installation commands as needed
+    if command -v wget >/dev/null 2>&1; then
+        echo "wget is installed. Using wget..."
+        wget -q --show-progress --max-redirect 5 -O "$output_filename" "$url"
+    elif command -v curl >/dev/null 2>&1; then
+        echo "wget is not installed. Using curl..."
+        curl -L --max-redirs 5 "$url" -o "$output_filename"
+    elif command -v wget2 >/dev/null 2>&1; then
+        echo "wget and curl are not installed. Using wget2..."
+        wget2 -q --progress=bar --max-redirect 5 -O "$output_filename" "$url"
+    else
+        echo "Neither wget, curl, nor wget2 is installed. Please install one of these tools to proceed."
+        return 1
     fi
 }
 
-if [ "$(uname)" = "Darwin" ]; then
-    # Do something under Mac OS X platform
-    install_latest "zvm-darwin-$ARCH.tar"
-elif [ $OS = "Linux" ]; then
-    # Do something under GNU/Linux platform
-    install_latest "zvm-linux-$ARCH.tar"
-elif [ $OS = "MINGW32_NT" ]; then
-    # Do something under 32 bits Windows NT platform
-    install_latest "zvm-windows-$ARCH.zip"
-elif [ $OS == "MINGW64_NT" ]; then
-    # Do something under 64 bits Windows NT platform
-    install_latest "zvm-windows-$ARCH.zip"
-fi
+install_zvm() {
+    local install_dir
+    local bin_dir
 
-echo
-echo "Run the following commands to put ZVM on your path via $HOME/.profile"
-echo
-# Check if TERM is set to a value that typically supports colors
-if [[ "$TERM" == "xterm" || "$TERM" == "xterm-256color" || "$TERM" == "screen" || "$TERM" == "tmux" ]]; then
-    # Colors
-    RED='\033[0;31m'   # For strings
-    GREEN='\033[0;32m' # For commands
-    BLUE='\033[0;34m'  # For variables
-    NC='\033[0m'       # No Color
+    case "$OS" in
+    "Darwin")
+        # MacOS platform
+        install_dir="$HOME/Library/zvm/self"
+        bin_dir="$HOME/bin"
+        ;;
+    "Linux")
+        # GNU/Linux platform
+        install_dir="${XDG_DATA_HOME:-$HOME/.local/share}/zvm/self"
+        bin_dir="$HOME/.local/bin"
+        ;;
+    *)
+        # Other OSes
+        install_dir="$HOME/.zvm"
+        bin_dir="$HOME/.zvm/bin"
+        ;;
+    esac
 
-    echo -e "${GREEN}echo${NC} ${RED}\"# ZVM\"${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC}"
-    echo -e "${GREEN}echo${NC} ${RED}'export ZVM_PATH=\"\$XDG_DATA_HOME/zvm\"'${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC} ${GREEN}# Linux${NC}"
-    echo -e "${GREEN}echo${NC} ${RED}'export ZVM_PATH=\"\$HOME/Library/zvm\"'${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC} ${GREEN}# macOS${NC}"
-    echo -e "${GREEN}echo${NC} ${RED}'export PATH=\"\$PATH:\$ZVM_PATH/bin\"'${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC}"
+    # Check if the installation directory already exists
+    if [ -d "$install_dir" ]; then
+        echo "Error: zvm is already installed. Please remove the directory $install_dir to reinstall."
+        return 1
+    fi
 
-    echo -e "Make sure ${RED}\$HOME/.local/bin${NC} is in your PATH on Linux."
-    echo -e "Make sure ${RED}\$HOME/bin${NC} is in your PATH on macOS."
+    download_file "https://github.com/tristanisham/zvm/releases/latest/download/$1" "zvm.zip"
 
-    echo "Run 'zvm i master' to install Zig"
-else
-    echo 'echo "# ZVM" >> $HOME/.profile'
-    echo 'echo '\''export ZVM_PATH="$XDG_DATA_HOME/zvm"'\'' >> $HOME/.profile # Linux'
-    echo 'echo '\''export ZVM_PATH="$HOME/Library/zvm"'\'' >> $HOME/.profile # macOS'
-    echo 'echo '\''export PATH="$PATH:$ZVM_PATH/bin"'\'' >> $HOME/.profile'
+    echo -e "Installing $1 in $install_dir"
+    mkdir -p "$install_dir"
+    tar -xf "zvm.zip" -C "$install_dir"
+    ln -sf "$install_dir/zvm" "$bin_dir"
+    rm "zvm.zip"
 
-    echo "Make sure \$HOME/.local/bin is in your PATH on Linux."
-    echo "Make sure \$HOME/bin is in your PATH on macOS."
+    post_install_setup
 
-    echo "Run 'zvm i master' to install Zig"
-fi
+}
 
-echo
+main() {
+    case "$ARCH" in
+    "aarch64")
+        ARCH="arm64"
+        ;;
+    "x86_64")
+        ARCH="amd64"
+        ;;
+    esac
+
+    if [ "$OS" != "Darwin" ] && [ "$OS" != "Linux" ]; then
+        echo "$OS-$ARCH not supported. If you are on Windows, please use install.ps1"
+        return 1
+    fi
+
+    echo "Installing zvm-${OS,,}-$ARCH.tar"
+    install_zvm "zvm-${OS,,}-$ARCH.tar"
+
+}
+
+main

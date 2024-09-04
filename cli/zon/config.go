7a53561d8ff982d8ef3bb2216d2c95850a613caa
Tristan Isham <tristan@isham.co>
2024-09-04 00:40:03 -0400

    Added basic depth parsing. Need to fix for mismatched braces

--- cli/zon/config.go
+++ cli/zon/config.go
@@ -29,7 +29,7 @@ func ParseConfig(reader io.Reader) (*Config, error) {
 		Dependencies: make(map[string]Dependency),
 	}
 
-    tokens := tokenize(scanner)
+	tokens := tokenize(scanner)
 	fmt.Println(tokens)
 
 	if err := scanner.Err(); err != nil {
@@ -41,7 +41,7 @@ func ParseConfig(reader io.Reader) (*Config, error) {
 
 // tokenize tokenizes a scanned zon file
 func tokenize(scanner *bufio.Scanner) []tc {
-    
+
 	tokens := make([]tc, 0)
 	depth := 0
 	cursor := 0
@@ -59,17 +59,15 @@ func tokenize(scanner *bufio.Scanner) []tc {
 			case '.':
 				tokens = append(tokens, tc{lexeme: ".", token: tkPeriod, depth: depth})
 			case '{':
+				depth++
 				tokens = append(tokens, tc{lexeme: "{", token: tkLeftBrace, depth: depth})
-				if tokens[len(tokens)-1].token == tkPeriod {
-					depth--
-				}
+            case '}':
+				tokens = append(tokens, tc{lexeme: "}", token: tkRightBrace, depth: depth})
+				depth--
 			case '=':
 				tokens = append(tokens, tc{lexeme: "=", token: tkEqual, depth: depth})
 			case ',':
 				tokens = append(tokens, tc{lexeme: ",", token: tkComma, depth: depth})
-				if tokens[len(tokens)-1].token == tkRightBrace {
-					depth++
-				}
 			case '"':
 				tokens = append(tokens, tc{lexeme: "\"", token: tkQuote, depth: depth})
 			case '_':
@@ -92,47 +90,46 @@ func tokenize(scanner *bufio.Scanner) []tc {
 	}
 	tokens = extractStrings(tokens)
 
-    return tokens
+	return tokens
 }
 
-
 func extractStrings(tokens []tc) []tc {
-    tks := make([]tc, 0)
-    for i := 0; i < len(tokens); i++ {
-        if tokens[i].token == tkLexeme {
-            var newStr string
-            var k int
-            quoted := false
-            
-            if i > 0 && tokens[i-1].token == tkQuote {
-                for j := i; j < len(tokens); j++ {
-                    if tokens[j].token != tkQuote {
-                        newStr += tokens[j].lexeme
-                    } else {
-                        k = j
-                        break
-                    }
-                }
-                quoted = true
-            } else {
-                for j := i; j < len(tokens); j++ {
-                    if tokens[j].token == tkLexeme || tokens[j].token == tkUnderscore {
-                        newStr += tokens[j].lexeme
-                    } else {
-                        k = j - 1 // Step back one to include the non-lexeme token
-                        break
-                    }
-                }
-            }
-            
-            tks = append(tks, tc{lexeme: newStr, token: tkLexeme, depth: tokens[i].depth})
-            if quoted {
-                tks = append(tks, tc{lexeme: "\"", token: tkQuote, depth: tokens[i].depth})
-            }
-            i = k
-        } else {
-            tks = append(tks, tokens[i])
-        }
-    }
-    return tks
+	tks := make([]tc, 0)
+	for i := 0; i < len(tokens); i++ {
+		if tokens[i].token == tkLexeme {
+			var newStr string
+			var k int
+			quoted := false
+
+			if i > 0 && tokens[i-1].token == tkQuote {
+				for j := i; j < len(tokens); j++ {
+					if tokens[j].token != tkQuote {
+						newStr += tokens[j].lexeme
+					} else {
+						k = j
+						break
+					}
+				}
+				quoted = true
+			} else {
+				for j := i; j < len(tokens); j++ {
+					if tokens[j].token == tkLexeme || tokens[j].token == tkUnderscore {
+						newStr += tokens[j].lexeme
+					} else {
+						k = j - 1 // Step back one to include the non-lexeme token
+						break
+					}
+				}
+			}
+
+			tks = append(tks, tc{lexeme: newStr, token: tkLexeme, depth: tokens[i].depth})
+			if quoted {
+				tks = append(tks, tc{lexeme: "\"", token: tkQuote, depth: tokens[i].depth})
+			}
+			i = k
+		} else {
+			tks = append(tks, tokens[i])
+		}
+	}
+	return tks
 }

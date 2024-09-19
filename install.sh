bc3d0ce622f15f32127e24e22f4b45cbdb37eaeb
Tristan Isham <tristan@isham.co>
2024-09-19 01:13:28 -0400

    remove comment

--- install.sh
+++ install.sh
@@ -118,33 +118,3 @@ else
         echo "Run 'zvm i master' to install Zig"
     fi
 fi
-
-# echo
-# echo "Run the following commands to put ZVM on your path via $HOME/.profile"
-# echo 
-# # Check if TERM is set to a value that typically supports colors
-# if [[ "$TERM" == "xterm" || "$TERM" == "xterm-256color" || "$TERM" == "screen" || "$TERM" == "tmux" ]]; then
-#     # Colors
-#     RED='\033[0;31m'        # For strings
-#     GREEN='\033[0;32m'      # For commands
-#     BLUE='\033[0;34m'       # For variables
-#     NC='\033[0m'            # No Color
-
-#     echo -e "${GREEN}echo${NC} ${RED}\"# ZVM\"${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC}"
-#     echo -e "${GREEN}echo${NC} ${RED}'export ZVM_INSTALL=\"\$HOME/.zvm/self\"'${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC}"
-#     echo -e "${GREEN}echo${NC} ${RED}'export PATH=\"\$PATH:\$HOME/.zvm/bin\"'${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC}"
-#     echo -e "${GREEN}echo${NC} ${RED}'export PATH=\"\$PATH:\$ZVM_INSTALL/\"'${NC} ${GREEN}>>${NC} ${BLUE}\$HOME/.profile${NC}"
-
-#     echo -e "Run '${GREEN}source ~/.profile${NC}' to start using ZVM in this shell!"
-#     echo "Run 'zvm i master' to install Zig"
-# else
-#     echo 'echo "# ZVM" >> $HOME/.profile'
-#     echo 'echo '\''export ZVM_INSTALL="$HOME/.zvm/self"'\'' >> $HOME/.profile'
-#     echo 'echo '\''export PATH="$PATH:$HOME/.zvm/bin"'\'' >> $HOME/.profile'
-#     echo 'echo '\''export PATH="$PATH:$ZVM_INSTALL/"'\'' >> $HOME/.profile'
-
-#     echo "Run 'source ~/.profile' to start using ZVM in this shell!"
-#     echo "Run 'zvm i master' to install Zig"
-# fi
-    
-# echo

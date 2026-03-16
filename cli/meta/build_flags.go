1edcabfc4c3851dcec49bb5895d99063a5942020
Tristan Isham <tristan@isham.co>
2026-03-16 19:36:21 -0400

    added support for build flags

---
+++ cli/meta/build_flags.go
@@ -0,0 +1,5 @@
+package meta
+
+// BuildFlag represent build flag arguments for program use.
+// They're really undercooked macros.
+type BuildFlag bool
---
+++ cli/meta/upgrade_disabled.go
@@ -0,0 +1,10 @@
+// This is the (dummy) fallback implementation to use if the `noAutoUpgrades` tag was passed for building.
+//go:build noAutoUpgrades
+
+package meta
+
+
+
+const (
+	NoAutoUpgrades BuildFlag = true
+)
